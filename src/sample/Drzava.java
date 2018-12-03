799ebfaed835920361b2a8b2828adf9bec2356e5
Amel <akarsic1@etf.unsa.ba>
2018-12-03 19:21:09 +0100

    Kreirana klasa Drzava.

---
+++ src/sample/Drzava.java
@@ -0,0 +1,52 @@
+package sample;
+
+public class Drzava {
+    private String naziv;
+    private int brojStanovnika;
+    private Double povrsina;
+    private String jedinicaZaPovrsinu;
+    private Grad glavniGrad;
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public Double getPovrsina() {
+        return povrsina;
+    }
+
+    public void setPovrsina(Double povrsina) {
+        this.povrsina = povrsina;
+    }
+
+    public String getJedinicaZaPovrsinu() {
+        return jedinicaZaPovrsinu;
+    }
+
+    public void setJedinicaZaPovrsinu(String jedinicaZaPovrsinu) {
+        this.jedinicaZaPovrsinu = jedinicaZaPovrsinu;
+    }
+
+    public Grad getGlavniGrad() {
+        return glavniGrad;
+    }
+
+    public void setGlavniGrad(Grad glavniGrad) {
+        this.glavniGrad = glavniGrad;
+    }
+
+
+
+}
