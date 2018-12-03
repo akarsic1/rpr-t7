f1c2cc427018c5404215acadad41d639779d35fd
Amel <akarsic1@etf.unsa.ba>
2018-12-03 19:22:50 +0100

    Kreirana klasa UN.

---
+++ src/sample/UN.java
@@ -0,0 +1,19 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class UN {
+    private ArrayList<Drzava> clanice;
+
+    public ArrayList<Drzava> getClanice() {
+        return clanice;
+    }
+
+    public void setClanice(ArrayList<Drzava> clanice) {
+        this.clanice = clanice;
+    }
+
+    public UN(ArrayList<Drzava> clanice) {
+        this.clanice = clanice;
+    }
+}
